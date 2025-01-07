6206bf21ea0a452b89845d51b9c7ab5d85d2c174
Diganta Das Droba <digantadas784@gmail.com>
2025-01-08 00:43:26 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [advice, setAdvice] = useState("");
+
+  const fetchAdvice = () => {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then((response) => {
+        const {advice} = response.data.slip;
+        setAdvice(advice);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  return (
+    <div className='app'>
+      <div className='card'>
+        <h1 className='heading'> {advice} </h1>
+        <button className='button' onClick={fetchAdvice}><span>GIVE ME ADVICE!</span></button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
